b0ec8b2fcb3c9cfcdcc0bb60a3bd97ec320ecbff
xReVaNx <kamil.p.rutyna@gmail.com>
2021-08-02 16:24:12 +0200

    Initil commit

---
+++ jkf.client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import style from './scss/Header.scss';
+
+function Header(){
+    return(
+        <>
+        <div className="container">
+            <div className="header">
+                Joanna Kretschmann Fotografia
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Header;
